bd37a88287d5780220063a3985ec9adb41d73183
Khai Tran <102895680@student.swin.edu.au>
2023-10-20 20:48:48 +1100

    Fix Runtime

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 from keras.models import load_model
 import cv2
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import openai
 import joblib
